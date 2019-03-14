24a04362316229c1278ca52afe380b3832d12b62
damtea <damtearlte@gmail.com>
2019-03-15 02:23:08 +0530

    Stream list show tawh

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,11 +12,10 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => {
-  return async dispatch => {
-    const response = await streams.post("/streams", formValues);
-    dispatch({ type: "CREATE_STREAM", payload: response.data });
-  };
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
+  dispatch({ type: "CREATE_STREAM", payload: response.data });
 };
 
 export const fetchStreams = () => {
