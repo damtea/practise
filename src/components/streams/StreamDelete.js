10f92339ec2d786f3af7ecc700c85fa90de67e76
damtea <damtearlte@gmail.com>
2019-03-18 00:25:43 +0530

    CRUD

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,27 +1,51 @@
 import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
-const StreamDelete = props => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button onClick={() => history.push("/")} className="ui button">
-        Cancel
-      </button>
-    </React.Fragment>
-  );
+import { connect } from "react-redux";
+import { fetchStream, deleteStream } from "../../actions";
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  renderActions = () => {
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <button onClick={() => history.push("/")} className="ui button">
+          Cancel
+        </button>
+      </React.Fragment>
+    );
+  };
 
-  return (
-    <div>
-      StreamDelete
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+    return `Are you sure you want to delete the stream with title: ${
+      this.props.stream.title
+    }`;
+  }
+  render() {
+    return (
       <Modal
         header="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        action={actions}
+        content={this.renderContent()}
+        action={this.renderActions()}
         onDismiss={() => history.push("/")}
       />
-    </div>
-  );
+    );
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-
-export default StreamDelete;
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
